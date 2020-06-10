33650f87229d5b3fd3d784850ec3b6cf1db8c623
PauloHenriqueLourenco <paulohenrilourenco@gmail.com>
2020-06-10 14:09:29 -0300

    Alteração controller users

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -3,34 +3,49 @@ const db = require("../database/connection");
 module.exports = {
   //lista usuarios
   async listAllUser(req, res) {
-    try {
-      const users = await db.select().from("users");
-      return res.json(users);
-    } catch (err) {
-      console.log("Erro na consulta.");
-      throw err;
-    }
+    const search = req.query.search;
+
+    const users = await db
+      .select(["users.*", "team.name_team"])
+      .count("action.id_user").as("amount_regis")
+      .from("users")
+      .join("team", "team.id_team", "=", "users.id_team")
+      .leftJoin("action", "action.id_user", "=", "users.id_user")
+      .groupBy(["users.id_user", "team.name_team"])
+      .where((qb) => {
+        if (search) {
+          // qb.orWhere("users.regis_user", "=", `${search}`);
+          qb.orWhere("users.nome_user", "ilike", `%${search}%`);
+          qb.orWhere("users.email_user", "ilike", `%${search}%`);
+          qb.orWhere("users.cargo_user", "ilike", `%${search}%`);
+          qb.orWhere("team.name_team", "ilike", `%${search}%`);
+        }
+      });
+
+    return res.json(users);
   },
   //insere no banco
   async createUser(req, res) {
     try {
       const {
-        regis_users,
-        name_users,
-        email_users,
-        password_users,
-        cargo_users,
-        urlimg_users,
+        regis_user,
+        nome_user,
+        email_user,
+        password_user,
+        cargo_user,
+        urlfoto_user,
+        id_team,
       } = req.body;
 
       const users = await db
         .insert({
-          regis_users,
-          name_users,
-          email_users,
-          password_users,
-          cargo_users,
-          urlimg_users,
+          regis_user,
+          nome_user,
+          email_user,
+          password_user,
+          cargo_user,
+          urlfoto_user,
+          id_team,
         })
         .into("users")
         .returning("*");
